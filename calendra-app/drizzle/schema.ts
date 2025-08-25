5201ca143edae24484aedfeb909d2cd8407b0736
Aaditya Raj Karki <karkiaaditya2@gmail.com>
2025-08-25 14:07:47 -0400

    database setup completed

--- calendra-app/drizzle/schema.ts
+++ calendra-app/drizzle/schema.ts
@@ -1,3 +1,5 @@
+import { DAYS_OF_WEEK_IN_ORDER } from "@/constants";
+import { relations } from "drizzle-orm";
 import { union } from "drizzle-orm/gel-core";
 import {
   boolean,
@@ -8,6 +10,7 @@ import {
   index,
   timestamp,
   unique,
+  pgEnum,
 } from "drizzle-orm/pg-core";
 
 const createdAt = timestamp("createdAt").notNull().defaultNow();
@@ -40,3 +43,37 @@ export const ScheduleTable = pgTable("schedules", {
   createdAt,
   updatedAt,
 });
+
+export const scheduleRelations = relations(ScheduleTable, ({ many }) => ({
+  availabilities: many(ScheduleAvailabilityTable),
+}));
+
+export const scheduleDayOfWeekEnum = pgEnum("day", DAYS_OF_WEEK_IN_ORDER);
+
+export const ScheduleAvailabilityTable = pgTable(
+  "scheduleAvailabilities",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    scheduleId: uuid("scheduleId")
+      .notNull()
+      .references(() => ScheduleTable.id, { onDelete: "cascade" }),
+
+    startTime: text("startTime").notNull(), // Minutes from midnight
+    endTime: text("endTime").notNull(), // Minutes from midnight
+    dayOfWeek: scheduleDayOfWeekEnum("dayOfWeek").notNull(), // 0 (Sunday) to 6 (Saturday)
+
+    createdAt,
+    updatedAt,
+  },
+  (table) => [index("scheduleIdIndex").on(table.scheduleId)]
+);
+
+export const scheduleAvailabilityRelations = relations(
+  ScheduleAvailabilityTable,
+  ({ one }) => ({
+    schedule: one(ScheduleTable, {
+      fields: [ScheduleAvailabilityTable.scheduleId],
+      references: [ScheduleTable.id],
+    }),
+  })
+);
