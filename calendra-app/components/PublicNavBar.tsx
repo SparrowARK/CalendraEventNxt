6438331acf1e4231e2da84b14151e46bb83332fe
Aaditya Raj Karki <karkiaaditya2@gmail.com>
2025-08-25 14:31:07 -0400

    Creating Public Navigation bar

---
+++ calendra-app/components/PublicNavBar.tsx
@@ -0,0 +1,41 @@
+import { SignUpButton } from "@clerk/clerk-react";
+import { SignInButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function PublicNavBar() {
+  return (
+    <nav
+      className="flex justify-between
+        items-center
+        fixed z-50
+        w-full h-28
+        bg-gray-300
+        px-10 gap-4
+        shadow-2xl"
+    >
+      <Link
+        href="/login"
+        className="flex items-center gap-1 hover:scale-150
+        duration-500"
+      >
+        <Image
+          src="/assets/logo.png"
+          width={60}
+          height={60}
+          alt="calendra logo"
+        />
+      </Link>
+      <section>
+        <div>
+          <SignInButton>
+            <Button></Button>
+          </SignInButton>
+          <SignUpButton>
+            <Button></Button>
+          </SignUpButton>
+        </div>
+      </section>
+    </nav>
+  );
+}
